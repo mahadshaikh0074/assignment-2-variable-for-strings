36f237e7fc51fb778e92d2617dfc3f630992a7ab
MAHAD <mahadshaikh0074>
2018-02-25 03:16:39 +0500

    assignment 2 variable for strings

---
+++ app.js
@@ -0,0 +1,50 @@
+// var userName;
+// var myName = "Mahad Farooq";
+// var messege = "Hello World"
+// alert(messege);
+
+var name = "Mahad Farooq"
+var age = "18 years old"
+var institute = "Saylani"
+var course = "Mobile And Cloud Computing"
+alert(name);
+alert(age);
+alert(institute);
+alert(course);
+
+
+
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+
+
+// var jobTItle = prompt("Enter your Job title");
+// var noOfChildren = prompt("Enter your childern");
+// var partner = prompt("Enter your Partner");
+// var location = prompt("Enter your location");
+// alert(jobTitle + noOfChildren + partner + location);
+
+
+
+// var email = "mahadshaikh667@gmail.com"
+// alert('you email address is ' + email);
+
+
+// var book = "'A smarter way to learn javascript'"
+// alert("I am trying to read Book " + book);
+
+
+
+alert("my name is " + name);
+alert("i am " + age);
+alert("i study in " + institute);
+alert("I learn " + course);
+
+
+
+
+
+
+
+
+document.write("yes! i can write in js");
